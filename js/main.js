b057956acf4486376c6ef47add2ec7a3d786b3cd
hafizumairfunsol <gmcreationsofficial@gmail.com>
2024-09-29 11:57:07 +0500

    new_revison

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 (function ($) {
-
     $(document).ready(function () {
-
         // sticky header --------
         function sticky_header(){
             var wind = $(window);
@@ -17,52 +15,47 @@
         }
         sticky_header();
 
-
         //  Back to top button --------
         $(window).on('scroll', function () {
             if ($(this).scrollTop() > 600) {
-                $('.back-to-top').fadeIn(200)
+                $('.back-to-top').fadeIn(200);
             } else {
-                $('.back-to-top').fadeOut(200)
+                $('.back-to-top').fadeOut(200);
             }
         });
-
 
         // animation for Back to top button --------
         $('.back-to-top').on('click', function (event) {
             event.preventDefault();
-
-            $('html, body').animate({
-                scrollTop: 0,
-            }, 1500);
+            $('html, body').animate({ scrollTop: 0 }, 1500);
         });
-
 
         // Hamburger-menu --------
         $('.hamburger-menu').on('click', function () {
-            $('.hamburger-menu .line-top, .ofcavas-menu').toggleClass('current');
-            $('.hamburger-menu .line-center').toggleClass('current');
-            $('.hamburger-menu .line-bottom').toggleClass('current');
+            // Toggle classes for hamburger icon
+            $('.hamburger-menu .line-top, .hamburger-menu .line-center, .hamburger-menu .line-bottom').toggleClass('current');
+            // Toggle ofcanvas menu visibility
+            $('.ofcavas-menu').toggleClass('current');
         });
 
+        // Close menu when a menu item is clicked
+        $('.ofcavas-menu a').on('click', function () {
+            // Close the menu
+            $('.hamburger-menu .line-top, .hamburger-menu .line-center, .hamburger-menu .line-bottom').removeClass('current');
+            $('.ofcavas-menu').removeClass('current'); // Also hide the menu
+        });
 
         // smartScroll -------------
         smartScroll.init({
-            speed: 700, // default 500
-            addActive: false, // default true
-            activeClass: "active", // default active
-            offset: 350 // default 100
+            speed: 700,
+            addActive: false,
+            activeClass: "active",
+            offset: 350
         }, function () {
             console.log("callback");
         });
 
-
         // wow animation --------------
         new WOW().init();
-
-
-
-        
     });
-
 })(jQuery);
